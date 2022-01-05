e37894748dda82832c640bbf5765f7b48d63ab27
Joshua <chojoshua99@gmail.com>
2022-01-04 18:37:07 -0800

    Finished basic functionality

--- script.js
+++ script.js
@@ -24,103 +24,99 @@ let getBook = (e) => {
     const {author, title, pages, read} = e.target.elements;
     let tempBook = new Book(author.value, title.value, pages.value, read.value)
     myLibrary.push(tempBook);
-    displayBook();
     toggleForm();
+    displayBook();
     getForm.reset();
+}
+
+//displays most recent book
+let displayBook = () => {
+    //display the last (ie most recent) book
+    let newBook = document.createElement("div");
+    let index = myLibrary.length - 1;
+    newBook.className = `card-${index}`; //set className === myLibrary index
+    let textContainer = document.createElement("div");
+    textContainer.className = "textAndRead";
+    displayText(textContainer, index);
+    newBook.append(textContainer);
+    bookContainer.appendChild(newBook);
+
+    //btn
+    let exitButton = document.createElement("button");
+    exitButton.id = "exit";
+    exitButton.innerHTML = "X";
+    newBook.appendChild(exitButton);
+    exitButton.addEventListener("click", exitFunction);
+
+    //switch if read
+    let readSwitch = document.createElement("label"); 
+    readSwitch.className = "switch";
+    readSwitch.innerText = "Mark as Read";
+    let checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    let slider = document.createElement("span");
+    slider.className = "slider round";
+    if(myLibrary[index].read === "yes"){
+        checkbox.checked = true;
+    }
+    else {
+        checkbox.checked = false;
+    }
+    readSwitch.appendChild(checkbox);
+    readSwitch.appendChild(slider);
+    textContainer.appendChild(readSwitch); //add read switch to card
+    changeRead(readSwitch, checkbox);
+}
+
+//change read status in myLibrary when switch is triggered
+let changeRead = (readSwitch, checkbox) =>{
+    readSwitch.addEventListener("click", function(event){
+        let cardIndex = event.target.parentElement.parentElement.className.substring(5);
+        let checker = checkbox.checked;
+        if(checker){
+            myLibrary[cardIndex].read = "yes";
+        }
+        else if(!checker){
+            myLibrary[cardIndex].read = "no";
+        }
+    });
+}
+
+let exitFunction = (e) => {
+    //when book is deleted
+    //  1) myLibrary updates
+    //  2) display updates
+    let cardIndex = e.target.parentElement.className.substring(5); //after 'card ' 
+    let card = document.querySelector(`.card-${cardIndex}`);
+    myLibrary.splice(cardIndex, 1); //remove the book from library
+    if(card.parentNode){
+        card.parentNode.removeChild(card);
+    }
+    for(let i = cardIndex; i < myLibrary.length;i++){
+        cardIndex++; //get pos of card (if there is one) one after deleted card
+        card = document.querySelector(`.card-${cardIndex}`); //nextCard
+        card.className = `card-${i}`; //make nextCard take pos of deleted card
+    }
+}
+
+//get newBook element and place text
+let displayText = (newBook, index) => {
+    let values = Object.values(myLibrary[index]); //gets value of title, author, etc
+    for(let i=0;i<values.length - 1;i++){ //adds these values to innerHTML with break
+        newBook.innerHTML += `${values[i]} <br>`;
+    }
+    newBook.style.textAlign = "center";
 }
 
 
 let toggleForm = () => {
     if(formContainer.style.display === "none"){
-        formContainer.style.display = "block";
+        formContainer.style.display = "inline-block";
     }
     else {
         formContainer.style.display = "none";
     }
 }
-/*
-
-*/
-let displayBook = () => {
-
-    let index = myLibrary.length-1;
-    let card = document.createElement("div")
-    card.className = `card`;
-    cardStyle(card);
-
-    //text container for below
-    let textBox = document.createElement("div");
-    textBox.className = "text-container";
-    //style
-    textBox.style.display = "flex";
-    textBox.style.flexDirection = "column";
-    textBox.style.justifyContent = "center";
-    textBox.style.alignItems = "center";
-    card.appendChild(textBox);
-
-    let values = Object.values(myLibrary[index]); // the hobbit, jrr martin, 295 pages, not read
-    let keys = Object.keys(myLibrary[index]); //title, author, etc.
-    //text for textbox
-    for(let i=0; i<keys.length;i++){ 
-        let val = values[i];
-        let text = document.createElement("div");
-        text.className = "info";
-        text.innerHTML = `${keys[i].toUpperCase()}: ${val}`;
-        if(i === 0){
-            text.style.fontSize = "36px";
-        }
-        else if(i === 1){
-            text.style.fontSize = "24px";
-        }
-        else {
-            text.style.fontSize = "20px";
-        }
-        textBox.appendChild(text);
-    }
-        
-    //exit button
-    let btn = document.createElement("button");
-    btn.id = "exit";
-    btn.innerHTML = "X";
-    btn.style.alignSelf = "flex-start";
-    card.appendChild(btn);
-
-    //read switch
-
-    //append card to container
-    bookContainer.appendChild(card);
-}
-
-let cardStyle = (card) => {
-    card.style.display = "flex";
-    //card.style.flexDirection = "column";
-    card.style.justifyContent = "space-between";
-    card.style.alignItems = "center";
-    card.style.border = "8px solid black";
-    card.style.padding = "20px";
-    card.style.borderRadius = "20px";
-    card.style.gap = "15px";
-}
-
-let readSwitch = (card) => {
-}
-
-let erase = (e) => {
-    console.log(e.target.id)
-    console.log(e.target.parentElement)
-    if(e.target.id === "exit"){
-        for(let i=0;i<bookContainer.children.length;i++){
-            if(bookContainer.children[i] === e.target.parentElement){
-                let child = bookContainer.children[i]; //select the selected card
-                bookContainer.removeChild(child); //remove the card from display
-                myLibrary.splice(i, 1); //remove the card from array
-                break;
-            }
-        }
-    }
-}
-
-bookContainer.addEventListener("click", erase)
-getForm.addEventListener("submit", getBook)
+formContainer.addEventListener("submit", getBook)
 newBookContainer.addEventListener("click", toggleForm)
 
